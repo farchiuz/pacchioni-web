8f5f51e8a9728663eead6f28d11e250bd4e07763
farchiuz <106334149+farchiuz@users.noreply.github.com>
2026-02-23 23:58:41 +0100

    Add box volume calculation in Es03a.js

---
+++ Es03a.js
@@ -0,0 +1,5 @@
+const larghezza =prompt("inserire la langhezza");
+const altezza = prompt("inserisci l'altezza");
+const lunghezza = prompt("inserisci lunghezza");
+const risultato= larghezza*altezza*lunghezza;
+console.log('la dimensione della scatola è: ${risultato}`);
